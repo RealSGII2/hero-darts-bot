08b0891ea7f55d785b55f7aa4ec0f49ad02d6774
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 07:59:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const client = new Discord.Client();
 const prefix = ":";
 
 client.on("ready", () => {
-  client.user.setActivity(`Helping ${client.guilds.size} servers.`)
+  client.user.setActivity(`Helping ${client.guilds.size} servers.`);
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); ;
 });
 
 client.on("guildCreate", guild => {
-  client.user.setActivity(`Helping ${client.guilds.size} servers.`)
+  client.user.setActivity(`Helping ${client.guilds.size} servers.`);
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Helping ${client.guilds.size} servers.`)
+  client.user.setActivity(`Helping ${client.guilds.size} servers.`);
 });
 
 client.on("message", async message => {
