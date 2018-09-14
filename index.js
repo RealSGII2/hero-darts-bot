276716b3244df547abec4fb5595a843f870e042f
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 13:25:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const config = "./config.json"
+                  
+client.on("ready", () => {
+ client.user.setActivity(`Ready!`)
+}
 
-// Command Manager
-client.on('message', message => {
-  // Command filtering
-  if (message.author.bot) return;
-  if (message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Argument setup
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  if (command === "ping") {
-    message.channel.send("Pong!")
-  }
-  
-});
-                           
 // Start the bot
 client.login(process.env.token);
