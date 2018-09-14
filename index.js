f35f7dbfb71fde0c09b4f7ec83f9c971f445f806
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 08:06:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const prefix = "*";
+const config = "./config.js"
 
 client.on("ready", () => {
   client.user.setActivity(`Helping ${client.guilds.size} servers.`);
