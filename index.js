b31d8fdaa94d704c98df789d311e5907bf370b95
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 08:08:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ client.on("guildDelete", guild => {
 client.on("message", async message => {
   // Command filtering
   if(message.author.bot) return;
-  if(message.content.indexOf(prefix) !== 0) return;
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   // Commands
