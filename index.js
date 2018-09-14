7f2b55eccb5760e97ec3144cdfe0e0b9fedf463e
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 11:15:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,16 @@ const client = new Discord.Client();
 
 const config = "./config.json"
 
-client.on("ready", () => {
-  client.user.setActivity(`Helping ${client.guilds.size} servers.`);
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); ;
-});
-
-client.on("guildCreate", guild => {
-  client.user.setActivity(`Helping ${client.guilds.size} servers.`);
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-});
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Helping ${client.guilds.size} servers.`);
-});
-
-client.on("message", async message => {
+// Command Manager
+client.on('message', message => {
   // Command filtering
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Argument setup
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  // Commands
-  
-  if (command === "ping") {message.channel.send("Pong!");}
 });
                            
 // Start the bot
