2f3b98c45bdb59c1e44d3a9cc02111e9a7726a67
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-13 23:20:12 -0700

    Delete base.js

--- utililities/base.js
+++
@@ -1,9 +0,0 @@
-exports.isMentionValid = function(mention) {
-    const leftMostIndex = mention.search('<');
-    const rightMostIndex = mention.search('>');
-    if(leftMostIndex == 0 && rightMostIndex == mention.length - 1) {
-        return true;
-    }
-
-    return false;
-};
