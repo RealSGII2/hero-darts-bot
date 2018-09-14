918a700f3fb15e76c86e286cb1d4e845e19a68ea
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 15:21:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
                   
 client.on("ready", () => {
- client.user.setActivity(`Ready!`)
+ client.user.setActivity(`Ready!`);
 }
 
 // Start the bot
