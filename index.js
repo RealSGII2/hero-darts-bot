18f8fa90593d6761aa82bb9432da34205f54f09f
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-14 07:51:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const config = require("./config.json");
+const prefix = ":"
 
 client.on("ready", () => {
-  client.user.setActivity(`:help | Helping ${client.guilds.size} servers.`)
+  client.user.setActivity(`Helping ${client.guilds.size} servers.`)
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); ;
 });
 
 client.on("guildCreate", guild => {
-  client.user.setActivity(`:help | Helping ${client.guilds.size} servers.`)
+  client.user.setActivity(`Helping ${client.guilds.size} servers.`)
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`:help | Helping ${client.guilds.size} servers.`)
+  client.user.setActivity(`Helping ${client.guilds.size} servers.`)
 });
 
 
@@ -23,48 +23,14 @@ client.login(process.env.token);
 
 
 client.on("message", async message => {
+  // Command filtering
   if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  if(message.content.indexOf(prefix) !== 0) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  if(command === "ping") {
-    const m = await message.channel.send("Pinging...");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
   
-  if (command === "help") {
-    message.channel.send("You got mail!");
-    message.author.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "Commands",
-    description: "Dart Bot Commands ",
-    fields: [{
-        name: "Prefix",
-        value: "The prefix for the bot is: `" + config.prefix + "`"
-      },
-      {
-        name: "Commands",
-        value: "You may view the bot commands [here](https://github.com/RealSGII2/Dart-s-Assistant/wiki/Commands)."
-      },
-      {
-        name: "Support Server",
-        value: "Join our [support server](https://discord.gg/zn4rbyn) if you need help!"
-      },
-      {
-        name: "Invite the bot!",
-        value: "Run `" + config.prefix + "invite` to get the box invite."
-      },
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "Requested"
-    }
-  }
-});
-  }
+  // Commands
+  
+  if (command === "ping") {message.channel.send("Pong!"}
+  
 });
